9386f0bf7aa7833f2eeabd47a3186747117c4349
Clotilde Mignon <clotilde.mignon@gmail.com>
2019-04-21 22:52:08 +0200

    add Files

---
+++ Assets/assets/menuStart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuStart : MonoBehaviour {
+
+
+	// Use this for initialization
+	public void changeScene(string scenename)
+    {
+        Application.LoadLevel(1); 
+    }
+
+    // Use this for initialization
+    public void changeScene2(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
